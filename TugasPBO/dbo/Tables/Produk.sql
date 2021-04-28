bbeb9476df4195131e7c6069725731d26c63bd4f
M Rafa F <rafa.dasw@mail.ugm.ac.id>
2021-04-29 06:26:47 +0700

    .

--- TugasPBO/dbo/Tables/Produk.sql
+++ TugasPBO/dbo/Tables/Produk.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Produk] (
     [ProdukId] INT        NOT NULL,
-    [Nama]     TEXT       NULL,
-    [Harga]    SMALLMONEY NULL,
-    [Deskripsi] TEXT NULL, 
+    [Nama]     NVARCHAR(100)       NULL,
+    [Alamat Email]    VARCHAR(150) NULL,
+    [Alamat] NVARCHAR(500) NULL, 
+    [Nomor HP] VARCHAR(250) NULL, 
+    [Jenis Kelamin] BIT NULL, 
     PRIMARY KEY CLUSTERED ([ProdukId] ASC)
 );
 
