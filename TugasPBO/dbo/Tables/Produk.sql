8658067e0d31b1458484f0b255d5913e9e708b72
M Rafa F <rafa.dasw@mail.ugm.ac.id>
2021-04-28 20:51:59 +0700

    Add project files.

---
+++ TugasPBO/dbo/Tables/Produk.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Produk] (
+    [ProdukId] INT        NOT NULL,
+    [Nama]     TEXT       NULL,
+    [Harga]    SMALLMONEY NULL,
+    [Deskripsi] TEXT NULL, 
+    PRIMARY KEY CLUSTERED ([ProdukId] ASC)
+);
+
